2302cdf4152167ee7ad5666dd990a120f4a79cb8
Cristiana Silva <cristianassilva10@gmail.com>
2024-05-30 23:28:52 +0100

    Added templates for listing the notes and add change to views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import StickyNote
 
 # Create your views here.
+
+def note_list(request):
+    notes = StickyNote.objects.all()
+
+    context ={
+        'notes': notes,
+        'note_title': 'List of Notes',
+    }
+
+    return render(request, 'notes/note_list.html', context)
