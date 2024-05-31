9c7d09a2186b9b84149c2f5500a86f9b07561331
Cristiana Silva <cristianassilva10@gmail.com>
2024-05-31 10:47:39 +0100

    Added note detail into view and urls

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import StickyNote
 
 # Create your views here.
 
+
 def note_list(request):
     notes = StickyNote.objects.all()
 
-    context ={
-        'notes': notes,
-        'note_title': 'List of Notes',
+    context = {
+        "notes": notes,
+        "note_title": "List of Notes",
     }
 
-    return render(request, 'notes/note_list.html', context)
+    return render(request, "notes/note_list.html", context)
+
+
+def note_detail(request, pk):
+    note = get_object_or_404(StickyNote, pk=pk)
+    return render(request, "notes/note_detail.html", {"note": note})
