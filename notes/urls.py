9e8b544f30fd6c184c052e41ddd9b34698463f8e
Cristiana Silva <cristianassilva10@gmail.com>
2024-05-31 14:51:14 +0100

    STN1 Added update functionaly and created note_form

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import note_list, note_detail
+from .views import note_list, note_detail, note_update
 
 urlpatterns = [
     path('', note_list, name='note_list'),
-    path('notes/<int:pk>/', note_detail, name='note_detail'),
+    path('note/<int:pk>/', note_detail, name='note_detail'),
+    path('note/<int:pk>/edit/', note_update, name='note_update'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import StickyNote
+from .forms import NoteForm
 
 # Create your views here.
 
@@ -18,3 +19,19 @@ def note_list(request):
 def note_detail(request, pk):
     note = get_object_or_404(StickyNote, pk=pk)
     return render(request, "notes/note_detail.html", {"note": note})
+
+def note_update(request, pk):
+    note = get_object_or_404(StickyNote, pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/note_form.html', {'form': form})
+
+
+
+
