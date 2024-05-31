a3c6d535ae6a190fc483d2f3463efec8fd32d7bd
Cristiana Silva <cristianassilva10@gmail.com>
2024-05-31 22:18:27 +0100

    Cleaned up the database

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import StickyNote, Author
+from .models import StickyNote
 
 # Register your models here.
 admin.site.register(StickyNote)
-admin.site.register(Author)
--- notes/models.py
+++ notes/models.py
@@ -8,11 +8,4 @@ class StickyNote(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
-    author = models.ForeignKey(
-        "Author", on_delete=models.CASCADE, null=True, blank=True
-    )
-
-
-class Author(models.Model):
-    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
